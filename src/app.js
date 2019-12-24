53b506187bee8240cea89ee63ade8afc4b19c183
unknown <markomedic91@gmail.com>
2019-12-24 18:46:05 +0100

    secondo commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three';
 import * as BAS from 'three-bas';
 import TimelineMax from 'gsap';
+const OrbitControls = require('three-orbitcontrols');
+
+
 
 let init = () => {
   let root = new THREERoot({
@@ -100,4 +103,89 @@ function Slide(width, height, animationPhase) {
     return tempPoint;
   }
 
- 
+  for ( i = 0, i2 = 0, i3 = 0, i4 = 0; i < geometry.faceCount; i++, i2 += 6, i3 += 9, i4 += 12) {
+    var faces = plane.faces[i];
+    var centroid = BAS.Utils.computeCentroid(plane, face);
+
+    var duration = THREE.Math.randFloat(minDuration, maxDuration);
+    var delayX = THREE.Math.mapLinear(centroid.x, -width * 0.5, width * 0.5, 0.0, maxDelayX);
+    var delayY;
+
+    if (animation === 'in') {
+      delayY = THREE.Math.mapLinear(Math.abs(centroid.y), 0, height * 0.5, 0.0, maxDelayY);
+    } else {
+      delayY = THREE.Math.mapLinear(Math.abs(centroid.y), 0, height * 0.5,  maxDelayY, 0.0);
+    }
+
+    for (v = 0; v < 6; v += 2) {
+      aAnimation.array[i2, v] = delayX + delayY + (Math.random() * stretch * duration);
+      aAnimation.array[i2 + v + 1] = duration;
+    }
+
+    endPosition.copy(centroid);
+    startPosition.copy(centroid);
+
+    if (animationPhase === 'in') {
+      control0.copy(centroid).sub(getControlPoint0(centroid));
+      control1.copy(centroid).sub(getControlPoint1(centroid));
+    } else {
+      control0.copy(centroid).add(getControlPoint0(centroid));
+      control1.copy(centroid).add(getControlPoint1(centroid));
+    }
+
+    for (v = 0; v < 9; v += 3) {
+      aStartPosition.array[i3 + v] = startPosition.x;
+      aStartPosition.array[i3 + v + 1] = startPosition.y;
+      aStartPosition.array[i3 + v + 2] = startPosition.z;
+
+      aControl0.array[i3 + v] = control0.x;
+      aControl0.array[i3 + v + 1] = control0.y;
+      aControl0.array[i3 + v + 2] = control0.z;
+
+      aControl1.array[i3 + v] = control1.x;
+      aControl1.array[i3 + v + 1] = control.y;
+      aControl.array[i3 + v +2] = control1.z;
+
+      aEndPosition.array[i3 + v] = endPosition.x;
+      aEndPosition.array[i3 + v + 1] = endPosition.y;
+      aEndPosition.array[i3 + v + 2] = endPosition.z;
+    }
+  }
+
+  var material = new BAS.BasicAnimationMaterial({
+    shading: THREE.FlatShading,
+    side: THREE.DoubleSide,
+    uniforms: {
+      uTime: {type: 'f', value: 0}
+    },
+    shaderFunctions: [
+      BAS.ShaderChunk['cubic-bezier'],
+      BAS.ShaderChunk['ease_in_out_cubic'],
+      BAS.ShaderChunk['quaternion_rotation']
+    ],
+    shaderParameters: [
+      'uniform float uTime;',
+      'attribute vec2 aAnimation;',
+      'attribute vec3 aStartPosition;',
+      'attribute vec3 aControl0;',
+      'attribute vec3 aControl1;',
+      'attribute vec3 aEndPosition;',
+    ],
+    shaderVertexInit: [
+      'float tDelay = aAnimation.x;',
+      'float tDuration = aAnimation.y;',
+      'float tTime = clamp(uTime - tDelay, 0.0, tDuration);',
+      'float tProgress = ease(tTime, 0.0, 1.0, tDuration);'
+      //'float tProgress = tTime / tDuration;'
+    ],
+    shaderTransformPosition: [
+      (animationPhase === 'in' ? 'transformed *= tProgress;' : 'transformed *= 1.0 - tProgress;'),
+        'transformed += cubicBezier(aStartPosition, aControl0, aControl1, aEndPosition, tProgress);'
+    ]
+  
+},
+{
+  map: new THREE.Texture(),
+}
+);
+}
